38aac45ad0323e02238740e48dafc595b3927184
NawaMan <nawa@dssbsoft.com>
2017-04-01 04:28:05 -0600

    Split the test for Get and Context.

---
+++ DirectGet/src/test/java/direct/get/ContextText.java
@@ -0,0 +1,59 @@
+package direct.get;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class ContextText {
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testProvidedWithAnotherType() {
+		Get.a(CurrentContext.class)
+			.provide(List.class).withA(ArrayList.class);
+		
+		List<String> list = Get.a(List.class);
+		assertTrue(list instanceof List);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testProvidedWithFixInstance() {
+		List<String> orgList = Arrays.asList("Hello", "World");
+		
+		Get.a(CurrentContext.class)
+			.provide(List.class).with(orgList);
+		
+		List<String> list = Get.a(List.class);
+		assertEquals(orgList, list);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testProvidedBySupplier() {
+		Get.a(CurrentContext.class)
+			.provide(List.class).by(()->Arrays.asList("Hello", "World"));
+		
+		List<String> list1 = Get.a(List.class);
+		List<String> list2 = Get.a(List.class);
+		assertEquals(list1, list2);
+		assertNotSame(list1, list2);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testProvidedByFunction() {
+		Get.a(CurrentContext.class)
+			.provide(List.class).by(clzz->Arrays.asList("Hello", "World"));
+		
+		List<String> list1 = Get.a(List.class);
+		List<String> list2 = Get.a(List.class);
+		assertEquals(list1, list2);
+		assertNotSame(list1, list2);
+	}
+
+}
--- DirectGet/src/test/java/direct/get/GetTest.java
+++ DirectGet/src/test/java/direct/get/GetTest.java
@@ -1,9 +1,9 @@
 package direct.get;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
@@ -24,42 +24,6 @@ public class GetTest {
 		
 		List<String> list = Get.a(List.class);
 		assertTrue(list instanceof List);
-	}
-	
-	@SuppressWarnings("unchecked")
-	@Test
-	public void testProvidedWithFixInstance() {
-		List<String> orgList = Arrays.asList("Hello", "World");
-		
-		Get.a(CurrentContext.class)
-			.provide(List.class).with(orgList);
-		
-		List<String> list = Get.a(List.class);
-		assertEquals(orgList, list);
-	}
-	
-	@SuppressWarnings("unchecked")
-	@Test
-	public void testProvidedBySupplier() {
-		Get.a(CurrentContext.class)
-			.provide(List.class).by(()->Arrays.asList("Hello", "World"));
-		
-		List<String> list1 = Get.a(List.class);
-		List<String> list2 = Get.a(List.class);
-		assertEquals(list1, list2);
-		assertNotSame(list1, list2);
-	}
-	
-	@SuppressWarnings("unchecked")
-	@Test
-	public void testProvidedByFunction() {
-		Get.a(CurrentContext.class)
-			.provide(List.class).by(clzz->Arrays.asList("Hello", "World"));
-		
-		List<String> list1 = Get.a(List.class);
-		List<String> list2 = Get.a(List.class);
-		assertEquals(list1, list2);
-		assertNotSame(list1, list2);
 	}
 
 }
